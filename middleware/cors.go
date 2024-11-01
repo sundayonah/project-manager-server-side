607ab4eea0825a770c75f2de88a3d5e252aca5bc
sundayonah <sundayonah94@gmail.com>
2024-11-01 08:35:36 +0100

    update: cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,14 +1,15 @@
 package middleware
 
 import (
-	"github.com/rs/cors"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func CORSMiddleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		corsMiddleware := cors.New(cors.Options{
-			AllowedOrigins: []string{"*"},
+			AllowedOrigins: []string{"http://localhost:3001"}, // Add your frontend URL here
 			AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 			AllowedHeaders: []string{"Content-Type", "Authorization", "X-Requested-With"},
 			ExposedHeaders: []string{"Content-Length"},
